3b4f789440ac51233f942114abbea07303248711
devendrakumar <101959297+Devendrabhuarya@users.noreply.github.com>
2024-02-05 09:34:11 +0530

    Update GuestRoute.jsx

--- client/src/Routes/GuestRoute.jsx
+++ client/src/Routes/GuestRoute.jsx
@@ -6,10 +6,7 @@ const GuestRoute = ({ children }) => {
     const { user,isAuth } = useSelector(state => state.auth);
     const navigate = useNavigate('/');
     useEffect(() => {
-          if(!user) {
-            localStorage.clear('token');
-            navigate('/');
-        }
+     
         if (isAuth) {
             navigate('/rooms');
         }
